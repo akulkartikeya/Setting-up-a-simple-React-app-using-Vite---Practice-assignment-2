d42b490ee8ec2cfa3dd186181fbba099453ae9a8
akulkarthikeya006@gmail.com <akulkarthikeya006@gmail.com>
2025-03-28 13:48:48 +0530

    m12

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Name: Akul kartikeya DEVAKIVADA </h1>
+      <h3> Contact. 72**-82-783</h3>
+      <h3>email: akulkartikeya006@gmail.com</h3>
+      <h3> Address: CTRI 61-4-14 Rajamundry,Andhrapradesh</h3>
+    </>
+  )
+}
+
+export default App
